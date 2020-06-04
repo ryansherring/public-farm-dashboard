385da3d10551405caa9d54d4448699716f97d477
Ryan <ryansherring@gmail.com>
2020-06-03 17:10:41 -0700

    Updates

---
+++ precache-manifest.7f0fac01f18b3c11313b3ea8cca197d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7984f0ba4da8ac304fe21cc6c04b19d1",
+    "url": "/public-farm-dashboard/index.html"
+  },
+  {
+    "revision": "d8b95c756832ac03343f",
+    "url": "/public-farm-dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0b1a32f672c7862e169d",
+    "url": "/public-farm-dashboard/static/css/main.acf01a45.chunk.css"
+  },
+  {
+    "revision": "d8b95c756832ac03343f",
+    "url": "/public-farm-dashboard/static/js/2.16e95944.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/public-farm-dashboard/static/js/2.16e95944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1a32f672c7862e169d",
+    "url": "/public-farm-dashboard/static/js/main.d9767725.chunk.js"
+  },
+  {
+    "revision": "9b56150d5b10408cb56b",
+    "url": "/public-farm-dashboard/static/js/runtime-main.116bb0f6.js"
+  }
+]);
